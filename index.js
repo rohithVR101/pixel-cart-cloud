13ab4e998f193328d68696577d752a78bbf220dc
Rohith Adithya <rohithriaz11@gmail.com>
2021-04-18 02:39:33 +0530

    Fix favicon 2

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
+app.use("/favicon.ico", express.static("public/images/favicon.ico"));
 
 app.use(
   session({
